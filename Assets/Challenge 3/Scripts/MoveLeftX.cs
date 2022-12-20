8190afda0253e59571ed8c556523c6a9ae83e3b2
w3villa-prince <prince.yadav@w3villa.com>
2022-12-20 16:00:10 +0530

    initial commit

---
+++ Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed;
+    private PlayerControllerX playerControllerScript;
+    private float leftBound = -50;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // If game is not over, move to the left
+        if (!playerControllerScript.gameOver)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        // If object goes off screen that is NOT the background, destroy it
+        if (transform.position.x < leftBound)//&& gameObject.CompareTag("Bomb")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
